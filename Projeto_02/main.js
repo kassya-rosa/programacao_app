76889ab866e0bbb3ecdef065afbad8383f024660
kassya-rosa <81445258+kassya-rosa@users.noreply.github.com>
2024-09-24 08:40:07 -0300

    Delete Projeto_02 directory

--- Projeto_02/main.js
+++
@@ -1,8 +0,0 @@
-function dados() {
-    let peso = parseFloat(prompt("Informe seu peso: "));
-    let altura = parseFloat(prompt("Informe sua altura: "));
-
-    let imc = peso / (altura * altura);
-
-    document.write(`<h2>Seu IMC é: ${imc.toFixed(2)}`);
-}
